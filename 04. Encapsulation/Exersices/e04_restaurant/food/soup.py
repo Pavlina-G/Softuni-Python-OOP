54424221e0cceb074a935989715000e8b2cd7d6d
Pavlina-G <102150683+Pavlina-G@users.noreply.github.com>
2022-07-10 16:51:57 +0300

    Adding classes to the food

---
+++ 04. Encapsulation/Exersices/e04_restaurant/food/soup.py
@@ -0,0 +1,6 @@
+from e04_restaurant.food.starter import Starter
+
+
+class Soup(Starter):
+    def __init__(self, name: str, price: float, grams: float):
+        super().__init__(name, price, grams)
