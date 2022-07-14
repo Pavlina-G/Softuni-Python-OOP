a3e4930032283d63fecd0095433589f554b36796
Pavlina-G <102150683+Pavlina-G@users.noreply.github.com>
2022-07-14 22:48:02 +0300

    Create e01_photo_album.py

---
+++ F05_Static_and_Class_Methods/Exercises/e01_photo_album.py
@@ -0,0 +1,35 @@
+from math import ceil
+
+
+class PhotoAlbum:
+    PHOTOS_PER_PAGE = 4
+
+    def __init__(self, pages):
+        self.pages = pages
+        self.photos = self.create_photos()
+
+    @classmethod
+    def from_photos_count(cls, photos_count: int):
+        pages = ceil(photos_count / PhotoAlbum.PHOTOS_PER_PAGE)
+        return cls(pages)
+
+    def add_photo(self, label: str):
+        for idx, page in enumerate(self.photos):
+            if len(page) < self.PHOTOS_PER_PAGE:
+                page.append(label)
+                return f"{label} photo added successfully on page {idx + 1} slot {len(page)}"
+        return "No more free slots"
+
+    def display(self):
+        delimiter = '-' * 11
+        result = delimiter
+        for page in self.photos:
+            page_info = ' '.join(['[]' for photo in page])
+            result += '\n' + page_info + "\n" + delimiter
+        return result.strip()
+
+    def create_photos(self):
+        result = []
+        for _ in range(self.pages):
+            result.append([] * self.PHOTOS_PER_PAGE)
+        return result
